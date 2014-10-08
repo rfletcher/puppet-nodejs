1b94984a3d0846c22417e3c842f0d449b9b74f44
Rick Fletcher <fletch@pobox.com>
2014-10-07 22:03:19 -0400

    Add nodejs::modules::jshint

---
+++ manifests/modules/jshint.pp
@@ -0,0 +1,21 @@
+# Class: nodejs::modules::jshint
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Usage:
+#
+class nodejs::modules::jshint (
+  $ensure = present,
+) {
+  include ::nodejs
+
+  package { 'jshint':
+    ensure   => $ensure,
+    provider => 'npm',
+    require  => Class['::nodejs'],
+  }  
+}
