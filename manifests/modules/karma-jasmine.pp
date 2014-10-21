360008975a46ed14af88c81d9cc4fb8a7dc519db
Rick Fletcher <fletch@pobox.com>
2014-10-21 17:27:16 -0400

    Added npm::modules::karma-jasmine

---
+++ manifests/modules/karma-jasmine.pp
@@ -0,0 +1,21 @@
+# Class: nodejs::modules::karma
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Usage:
+#
+class nodejs::modules::karma-jasmine (
+  $ensure = present,
+) {
+  include ::nodejs
+
+  package { 'karma-jasmine':
+    ensure   => $ensure,
+    provider => 'npm',
+    require  => Class['::nodejs'],
+  }  
+}
