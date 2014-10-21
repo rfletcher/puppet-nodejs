285e0bd4a3ebbf520388b61ef85f81bdf880a559
Rick Fletcher <fletch@pobox.com>
2014-10-21 17:35:30 -0400

    Fix ordering of karma and its link

--- manifests/modules/karma.pp
+++ manifests/modules/karma.pp
@@ -22,8 +22,9 @@ class nodejs::modules::karma (
   # annoyingly, karma doesn't link itself into the path like other npm modules
   if $ensure == 'present' {
     file { '/usr/bin/karma':
-      ensure => link,
-      target => '/usr/lib/node_modules/karma/bin/karma',
+      ensure  => link,
+      target  => '/usr/lib/node_modules/karma/bin/karma',
+      require => Package['karma'],
     }
   } else {
     file { '/usr/bin/karma':
